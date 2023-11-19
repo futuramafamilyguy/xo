e72492e68137f555d6f6ae670f9edc05c868f4ca
allen <awan968@aucklanduni.ac.nz>
2023-11-19 21:56:45 +1300

    implemented actually playing the game

--- xo.c
+++ xo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_OF_PLAYERS 2
 #define NUM_OF_SQUARES 9
 #define BOARD_DIM 3
 
@@ -10,39 +11,55 @@ typedef struct {
 }Player;
 
 typedef struct {
-    int x, y, id;
+    int x, y, pos;
     Player *player;
 }Sq;
 
 Sq* init_squares();
 
+Player* init_players();
+
 void print_grid(Sq *squares);
 
-char get_square_value(Sq* square);
+char get_square_value(Sq *square);
+
+int prompt_user(Player *currentPlayer, int *pos);
+
+int execute_move(Player *currentPlayer, Sq *squares, int pos);
 
 int main() {
     
     // instantiate sq
     Sq *squares = init_squares();
-    print_grid(squares);
+
     // instantiate players
+    Player *players = init_players();
 
-    // do
-    //     print board
+    int turnCounter = 0;
+    int inputPos;
 
-    //     determine player turn
+    do {
+        print_grid(squares);
 
-    //     player move
+        if (prompt_user(&players[turnCounter], &inputPos)) {
 
-    // while (check if game over)
+            if (execute_move(&players[turnCounter], squares, inputPos)) {
+
+                turnCounter = 1 - turnCounter;
+            }
+        }
+        
+    } while (1);
 
     // clean up
     free(squares);
+    free(players);
 
     return 0;
 }
 
 Sq* init_squares() {
+
     Sq *squares = malloc(NUM_OF_SQUARES * sizeof(Sq));
 
     for (int i = 0; i < BOARD_DIM; i++) {
@@ -50,14 +67,63 @@ Sq* init_squares() {
             int count = i * BOARD_DIM + j;
             squares[count].x = i;
             squares[count].y = j;
-            squares[count].id = count + 1;
+            squares[count].pos = count + 1;
         }
     }
 
     return squares;
 }
 
+Player* init_players() {
+
+    Player *players = malloc(NUM_OF_PLAYERS * sizeof(Player));
+    players[0].id = 1;
+    players[0].symbol = 'X';
+    players[1].id = 2;
+    players[1].symbol = 'O';
+
+    return players;
+}
+
+int prompt_user(Player *currentPlayer, int *pos) {
+
+    printf("player %d's turn. enter square to place mark (1-9): ", currentPlayer->id);
+
+    char input, first;
+    int count = 0;
+
+    first = fgetc(stdin);
+    while (((input = fgetc(stdin)) != '\n') && (input != EOF)) {
+        count++;
+    }
+
+    if (count > 0 || (first < '1') || (first > '9')) {
+        printf("invalid input. try again.\n");
+
+        return 0;
+    }
+
+    *pos = first - '0';
+    
+    return 1;
+}
+
+int execute_move(Player *currentPlayer, Sq *squares, int pos) {
+
+    if (squares[pos - 1].player) {
+        
+        printf("square %d is already marked. try again\n", pos);
+
+        return 0;
+    }
+
+    squares[pos - 1].player = currentPlayer;
+
+    return 1;
+}
+
 void print_grid(Sq *squares) {
+
     char values[NUM_OF_SQUARES];
 
     for (int i = 0; i < NUM_OF_SQUARES; i++) {
@@ -76,5 +142,11 @@ void print_grid(Sq *squares) {
 }
 
 char get_square_value(Sq* square) {
-    return square->id + '0';
+
+    if (square->player) {
+
+        return square->player->symbol;
+    }
+
+    return square->pos + '0';
 }
